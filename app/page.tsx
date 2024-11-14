fbb6fcefd8639c73b764965b4fd5528dcb4e5e38
pawansuthar <pawansuthar15372097023627153@gmail.com>
2024-11-14 15:52:26 +0530

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col  max-h-screen">
+      <h1>HOme page</h1>
+    </main>
+  );
+}
